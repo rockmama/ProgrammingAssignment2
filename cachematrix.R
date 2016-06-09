b9a27034edb999dfb0c6ccd29021592703cace84
Neha Tekriwal <singh.vaibhav033@gmail.com>
2016-06-09 18:15:58 +0530

    Assignment completed

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,19 @@
 ## functions do
 
 ## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  cachesolve(matrix)
+  
 }
-
-
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{  if(!is.null(x))
+{
+  message("getting cached data")
+  return(x)
+}
+  sol <- solve(x)
+  return(sol)
+  ## Return a matrix that is the inverse of 'x'
 }
